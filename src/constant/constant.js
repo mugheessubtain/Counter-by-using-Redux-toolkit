47567fc0af303033b205c49a18ba2986116e0106
Mughees Subtain <160583245+mugheessubtain@users.noreply.github.com>
2024-12-25 23:55:47 -0800

    Update constant.js

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -1,13 +1,1 @@
 
-const DEV_URL = `http://localhost:4000/`
-const PROD_URL = `http://localhost:4000/`
-
-
-export const BASE_URL = DEV_URL
-
-export const ApiRoutes = {
-    login: BASE_URL + 'auth/login',
-    register: BASE_URL + 'auth/register',
-    getTask: BASE_URL + 'task',
-    postTask: BASE_URL + 'task',
-}
